7846f08178e203c628e410325d9ef4898955e364
manasesortez <manasesortez1234@hotmail.com>
2021-08-21 17:39:18 -0600

    u20171110

---
+++ app/src/main/java/com/amtodev/calculator_metric/Calculator.java
@@ -0,0 +1,17 @@
+package com.amtodev.calculator_metric;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Calculator extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+
+    }
+}
---
+++ app/src/main/java/com/amtodev/calculator_metric/MainActivity.java
@@ -0,0 +1,32 @@
+package com.amtodev.calculator_metric;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TimerTask Splash = new TimerTask(){
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Calculator.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        Timer time = new Timer();
+        time.schedule(Splash, 4000);
+    }
+
+}
